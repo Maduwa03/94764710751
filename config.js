e46e0168cc41e5ad1f7278b64865382da222bd54
Maduwa03 <maduwawml@gmail.com>
2025-06-23 07:44:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ const fsm = require('fs'); if (fsm.existsSync('config.env')) require('dotenv').c
 ╚═╝░░░░░╚═╝╚═════╝░ 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗠𝗥 𝗗𝗜𝗟𝗔 𝗢𝗙𝗖
 */
 
-SESSION_ID: process.env.SESSION_ID || "", 
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "MONEY-HEIST-MD-yAFlWYAA#MmER1jpMF8DukpJvYbw6faHT6fkaamngAoG6yHwRjyM-DILALK", 
+MONGODB: process.env.MONGODB || "mongodb+srv://atlas-sample-dataset-load-67e42de564d2e7596ec5e5b2:8mqXCQYYPEKXDaS2@cluster0.80odkdc.mongodb.net/",
 
 
 
